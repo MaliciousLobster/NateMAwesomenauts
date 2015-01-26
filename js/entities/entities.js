790a96e29b473a40c8045928ec9852f6c55c55b9
NateM <Mr.Awsmness@gmail.com>
2015-01-26 12:08:11 -0800

    video  10, animating the player CODE

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,6 +13,11 @@ game.PlayerEntity = me.Entity.extend ({
 
 		this.body.setVelocity(5, 20); //moves five units right
 
+		this.renderable.addAnimation("idle", [78]);
+		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+
+		this.renderable.setCurrentAnimation("idle");
+
 	},
 
 	update: function(delta){
@@ -21,15 +26,24 @@ game.PlayerEntity = me.Entity.extend ({
 			//it by me.timer.tick
 			//me.time.tick makes movement smooth
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.flipX(true);
 		}else if(me.input.isKeyPressed("left")){
 			this.body.vel.x -= this.body.accel.x * me.timer.tick;
 		}
 		else{
-
 			this.body.vel.x = 0;
+			this.renderable.setCurrentAnimation("idle");	
+		}
+
+		if(this.body.vel.x !== 0) {
+			if(!this.renderable.isCurrentAnimation("walk")){
+				this.renderable.setCurrentAnimation("walk");
+			}
 		}
 
 		this.body.update(delta); //delta is the change in time
+
+		this._super(me.Entity, "update", [delta]);
 		return true;
 	}
 });
