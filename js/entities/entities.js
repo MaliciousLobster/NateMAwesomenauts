e165a067b5a0b6f4aab66ed9ab3f886bc5405b09
NateM <Mr.Awsmness@gmail.com>
2015-02-05 11:26:20 -0800

    video 16, top of tower collision CODED + COMMENTED

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -83,7 +83,12 @@ game.PlayerEntity = me.Entity.extend ({
 			var ydif = this.pos.y - response.b.pos.y; //the difference between the base's y and the player's y
 			var xdif = this.pos.x - response.b.pos.x; //the difference between the base's x and the player's x
 
-			if(xdif>-35  && this.facing === 'right' && (xdif < 0)){
+
+			if(ydif<-40 && xdif<60 && xdif>-35){
+				this.body.falling = false;
+				this.body.vel.y = -1;
+			}
+			else if(xdif>-35  && this.facing === 'right' && (xdif < 0)){
 				this.body.vel.x = 0;
 				this.pos.x = this.pos.x - 1;
 			}else if(xdif<60 && this.facing === 'left' && (xdif > 0)){
