03e81106076d0d76d00ae5ee9a046221f2722666
Nate M <Mr.awsmness@gmail.com>
2015-03-04 11:10:05 -0800

    video 40, added Continue, CODED + COMMENTED

--- js/screens/title.js
+++ js/screens/title.js
@@ -7,7 +7,7 @@ game.TitleScreen = me.ScreenObject.extend({
 
 		
 
-		me.game.world.addChild(new (me.Renderable.extend({
+		me.game.world.addChild(new (me.Renderable.extend({ //Starts a new game, resets all variables
 			init: function(){
 				this._super(me.Renderable, 'init',[270, 240, 300, 50]);
 				this.font = new me.Font("Comic Sans MS", 46, "white"); //sets the font
@@ -35,6 +35,28 @@ game.TitleScreen = me.ScreenObject.extend({
 				me.save.remove('exp4');	
 			}
 		})));
+
+		me.game.world.addChild(new (me.Renderable.extend({ //'continues' game. does same as above but doesn't remove variables
+			init: function(){
+				this._super(me.Renderable, 'init',[380, 340, 250, 50]);
+				this.font = new me.Font("Comic Sans MS", 46, "white"); //sets the font
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true); //when you click on the mouse, set to true
+			},
+
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y); //puts "Such Awesomenauts" at those coordinates
+				
+			},
+
+			update: function(dt){
+				return true;
+			},
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				me.state.change(me.state.PLAY); //changes state to PLAY
+			},
+		})));
+
 
 
 	},
