5f68c7a12f5c4fddb2ed7a1e934f119fd4813f66
Nate M <Mr.awsmness@gmail.com>
2015-03-09 12:10:33 -0700

    Video 43, adding more text to SpendExp screen, CODED (no need for comments)

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -9,13 +9,17 @@ game.SpendExp = me.ScreenObject.extend({
 
 		me.game.world.addChild(new (me.Renderable.extend({ //Starts a new game, resets all variables
 			init: function(){
-				this._super(me.Renderable, 'init',[270, 240, 300, 50]);
+				this._super(me.Renderable, 'init',[10, 10, 300, 50]);
 				this.font = new me.Font("Comic Sans MS", 46, "white"); //sets the font
 			},
 
 			draw: function(renderer){
-				this.font.draw(renderer.getContext(), "SPEND EXP", this.pos.x, this.pos.y); //puts "Such Awesomenauts" at those coordinates
-				
+				this.font.draw(renderer.getContext(), "PRESS 1-4 TO BUY, PRESS ENTER TO SKIP", this.pos.x, this.pos.y); //puts "Such Awesomenauts" at those coordinates
+				this.font.draw(renderer.getContext(), "CURRENT EXP: " + game.data.exp.toString(), this.pos.x , this.pos.y + 50); //puts "Such Awesomenauts" at those coordinates
+				this.font.draw(renderer.getContext(), "1 - GAIN MORE GOLD " + game.data.exp.toString(), this.pos.x , this.pos.y + 100); //puts "Such Awesomenauts" at those coordinates
+				this.font.draw(renderer.getContext(), "2 - +DAMAGE " + game.data.exp.toString(), this.pos.x , this.pos.y + 150); 
+				this.font.draw(renderer.getContext(), "3 - +HEALTH " + game.data.exp.toString(), this.pos.x , this.pos.y + 200); 
+				this.font.draw(renderer.getContext(), "4 - +ATTACK SPEED " + game.data.exp.toString(), this.pos.x , this.pos.y + 250); 
 			}
 		})));
 	},
