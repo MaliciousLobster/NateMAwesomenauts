f2c95d17dfa568c6d8461cc73848614018833787
Nate M <Mr.awsmness@gmail.com>
2015-03-11 10:57:21 -0700

    video 47, drawing spendGold image, CODED + COMMENTED

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -80,8 +80,34 @@ game.SpendGold = Object.extend({
 		this.lastBuy = new Date().getTime(); //keeps track of the last time a creep happened
 		this.paused = false;
 		this.alwaysUpdate = true;
+		this.updateWhenPaused = true;
 	},
 	update: function(){
+		this.now = new Date().getTime();//a timer
+		if(me.input.isKeyPressed("buy") && this.now-this.lastBuy >= 1000){
+			this.lastBuy = this.now;
+			if(!this.buying){
+				this.startBuying();
+			}else{
+				this.stopBuying();
+			}
+		}	
 		return true;
 	},
+	startBuying: function(){
+		this.buying = true;
+		me.state.pause(me.state.PLAY);
+		game.data.pausePos = me.game.viewport.localToWorld(0, 0); //sets to current location, keeps track of player's position
+		game.data.buyscreen	 = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage("gold-screen")); //sets up the  gold screen
+		game.data.buyscreen.updateWhenPaused = true;
+		game.data.buyscreen.setOpacity(0.85);
+		me.game.world.addChild(game.data.buyscreen, 34);//adds buy screen
+		game.data.player.body.setVelocity(0, 0); //makes player stop moving
+	},
+	stopBuying: function(){
+		this.buying = false;
+		me.state.resume(me.state.PLAY);
+		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20); //returns the player's movement
+		me.game.world.removeChild(game.data.buyscreen); //removes the buyscreen
+	}
 });
