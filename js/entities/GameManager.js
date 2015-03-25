464e6ed6d5d847f2fcd2805d75f4d2588d2f2e38
Nate M <Mr.awsmness@gmail.com>
2015-03-25 11:15:47 -0700

    video 50, making purchases, CODED + COMMENTED

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -101,7 +101,9 @@ game.SpendGold = Object.extend({
 			}else{
 				this.stopBuying();
 			}
-		}	
+		}
+		this.checkBuyKeys();
+
 		return true;
 	},
 	startBuying: function(){
@@ -156,5 +158,89 @@ game.SpendGold = Object.extend({
 		me.input.unbindKey(me.input.KEY.F5, "F5", true);
 		me.input.unbindKey(me.input.KEY.F6, "F6", true);
 		me.game.world.removeChild(game.data.buytext);
+	},
+	checkBuyKeys: function(){
+		if(me.input.isKeyPressed("F1")){ //checking to see if you have enough to buy something
+			if(this.checkCost(1)){
+				this.makePurchase(1);
+			}
+		}else if(me.input.isKeyPressed("F2")){
+			if(this.checkCost(2)){
+				this.makePurchase(2);
+			}
+		}else if(me.input.isKeyPressed("F3")){
+			if(this.checkCost(3)){
+				this.makePurchase(3);
+			}
+		}else if(me.input.isKeyPressed("F4")){
+			if(this.checkCost(4)){
+				this.makePurchase(4);
+			}
+		}else if(me.input.isKeyPressed("F5")){
+			if(this.checkCost(5)){
+				this.makePurchase(5);
+			}
+		}else if(me.input.isKeyPressed("F6")){
+			if(this.checkCost(6)){
+				this.makePurchase(6);
+			}
+		}
+	},
+
+	checkCost: function(skill){
+		if(skill1===1 && (game.data.gold >= ((game.data.skill1 + 1) * 10))){ //if skill1 was selected and I have enough gold to level up skill1, return true
+
+		}else{
+			return false;
+		}
+		if(skill2===2 && (game.data.gold >= ((game.data.skill2 + 1) * 10))){ //if skill2 was selected and I have enough gold to level up skill2, return true
+
+		}else{
+			return false;
+		}
+		if(skill3===3 && (game.data.gold >= ((game.data.skill3 + 1) * 10))){ //if skill3 was selected and I have enough gold to level up skill3, return true
+
+		}else{
+			return false;
+		}
+		if(skill4===4 && (game.data.gold >= ((game.data.ability1 + 1) * 10))){ //if skill4 was selected and I have enough gold to level up skill4, return true
+
+		}else{
+			return false;
+		}
+		if(skill5===5 && (game.data.gold >= ((game.data.ability2 + 5) * 10))){ //if skill5 was selected and I have enough gold to level up skill5, return true
+
+		}else{
+			return false;
+		}
+		if(skill6===6 && (game.data.gold >= ((game.data.ability3 + 6) * 10))){ //if skill6 was selected and I have enough gold to level up skill6, return true
+
+		}else{
+			return false;
+		}
+	},
+
+	makePurchase: function(skill){
+		if(skill===1){
+			game.data.gold -= ((game.data.skill1 + 1)*10);
+			game.data.skill1 += 1;
+			game.data.player.attack += 1;
+		}else if(skill2===2){
+			game.data.gold -= ((game.data.skill2 + 1)*10);
+			game.data.skill1 += 1;
+		}else if(skill2===3){
+			game.data.gold -= ((game.data.skill3 + 1)*10);
+			game.data.skill1 += 1;
+		}else if(ability1===4){
+			game.data.gold -= ((game.data.ability1 + 1)*10);
+			game.data.skill1 += 1;
+		}else if(ability2===5){
+			game.data.gold -= ((game.data.ability2 + 1)*10);
+			game.data.skill1 += 1;
+		}else if(ability3===6){
+			game.data.gold -= ((game.data.ability3 + 1)*10);
+			game.data.skill1 += 1;
+		}
+
 	}
 });
