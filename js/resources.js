5b5cdbcab8b9a371f4d2d4a488f838016040fc44
Nate M <Mr.awsmness@gmail.com>
2015-03-26 11:28:55 -0700

    Video 52, new and load screens, CODED + COMMENTED errors loading loadProfile

--- js/resources.js
+++ js/resources.js
@@ -8,6 +8,8 @@ game.resources = [
 	{name: "title-screen", type:"image", src: "data/img/title.png"},
 	{name: "exp-screen", type:"image", src: "data/img/loadpic.png"},
 	{name: "gold-screen", type:"image", src: "data/img/spend.png"},
+	{name: "new-screen", type:"image", src: "data/img/newpic.png"},
+	{name: "old-screen", type:"image", src: "data/img/loadpic.png"},
 	
 	/* Graphics. 
 	 * @example
--- js/screens/title.js
+++ js/screens/title.js
@@ -28,13 +28,9 @@ game.TitleScreen = me.ScreenObject.extend({
 				me.state.change(me.state.PLAY); //changes state to PLAY
 			},
 			removeVar: function(){
-				me.save.remove('exp');
-				me.save.remove('exp1');
-				me.save.remove('exp2');
-				me.save.remove('exp3');
-				me.save.remove('exp4');	
-				me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0,});
-				me.state.change(me.state.PLAY);
+
+				
+				me.state.change(me.state.NEW);
 			}
 		})));
 
@@ -54,14 +50,8 @@ game.TitleScreen = me.ScreenObject.extend({
 				return true;
 			},
 			newGame: function(){
-				game.data.exp = me.save.exp; //saves exp variable
-				game.data.exp1 = me.save.exp1;
-				game.data.exp2 = me.save.exp2;
-				game.data.exp3 = me.save.exp3;
-				game.data.exp4 = me.save.exp4;
-
 				me.input.releasePointerEvent('pointerdown', this);
-				me.state.change(me.state.SPENDEXP); //changes state to PLAY
+				me.state.change(me.state.LOAD); //changes state to PLAY
 			},
 		})));
 
